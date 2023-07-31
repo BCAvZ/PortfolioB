cf91e9c5f8e9f752b5b92eec453278c5df3b4360
BCAvZ <Bartvanzanen@hotmail.com>
2023-07-31 16:23:50 +0200

    Fixed layout changes with a media query for 640px+ below remains adaptable and susceptible to layout changes with different images, but impact on these screens is minimal. To do: contact page

--- src/components/ProjectInfo/ProjectInfo.js
+++ src/components/ProjectInfo/ProjectInfo.js
@@ -3,13 +3,13 @@ import React from 'react';
 export function ProjectInfo({selectedProject, setSelectedProject, title, link, picture, description}) {
 
     return (
-            <article className="bg-amber-100 rounded-2xl p-2">
+            <article className="bg-amber-100 rounded-2xl p-2 grow-0">
                 <h1 className="font-bold flex justify-center">{title}</h1>
-                <div className="flex flex-row justify-center items-center">
+                <div className="flex flex-row justify-evenly items-center">
                     <button onClick={(e) => setSelectedProject(1)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Vorige</button>
-                    <div className="flex justify-center">
-                        <a href={link} className="w-3/5 h-auto">
-                            <img src={picture} alt="MixB"/>
+                    <div className="flex justify-center sm:h-96 sm:w-96 h-2/4 w-2/4 ">
+                        <a href={link} className=" ">
+                            <img src={picture} alt="MixB" className="w-full h-full object-fill"/>
                         </a>
                     </div>
                     <button onClick={(e) => setSelectedProject(2)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Volgende</button>
