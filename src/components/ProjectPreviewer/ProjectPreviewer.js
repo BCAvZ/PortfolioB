4172a80352b44f7fa08451ceec4d9bca60c9f950
BCAvZ <Bartvanzanen@hotmail.com>
2023-10-30 22:24:47 +0100

    json filled to do fix crash of ProjectPreviewer component + finish english version of app

--- src/components/ProjectPreviewer/ProjectPreviewer.js
+++ src/components/ProjectPreviewer/ProjectPreviewer.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import MixB from '../../assets/pictures/Preview_image_MixB.PNG'
+import MixB from '../../assets/pictures/MixB.PNG'
 import portfolio from '../../assets/pictures/portfolio_background_Dutch.png'
-
+import x from "../../assets/pictures/MixB.PNG"
 
 export function ProjectPreviewer({selectedProject, setSelectedProject, title, link, picture, description}) {
+
+    console.log({picture})
+
+    function addOrReduce(amount){
+        if(selectedProject <= 2 && selectedProject>= 0) {
+            // console.log(selectedProject)
+            // setSelectedProject(selectedProject + amount)
+
+            setSelectedProject((prevSelectedProject) => {
+                console.log(prevSelectedProject);
+                return prevSelectedProject + amount;
+            });
+
+        } else {
+            console.log(selectedProject)
+            setSelectedProject (selectedProject - amount)
+        }
+    }
 
     return (
             <article className="bg-amber-100 rounded-2xl p-2 grow-0">
                 <h1 className="font-bold flex justify-center">{title}</h1>
                 <div className="flex flex-row justify-evenly items-center">
-                    <button onClick={(e) => setSelectedProject(1)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Vorige</button>
+                    <button onClick={(e) => addOrReduce(-1)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Vorige</button>
                     <div className="flex justify-center sm:h-96 sm:w-96 h-2/4 w-2/4 ">
                         <a href={link} className=" ">
-                            <img src={picture} alt="MixB" className="w-full h-full object-fill"/>
+                            <img src={`../../assets/pictures/${picture}`} alt="ApplicationPreview" className="w-full h-full object-fill"/>
                         </a>
                     </div>
-                    <button onClick={(e) => setSelectedProject(2)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Volgende</button>
+                    <button onClick={(e) => addOrReduce(1)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Volgende</button>
                 </div>
                 <p className=" max-w-2xl italic">
                     <a href={link} className="underline">{title}</a> {description}
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,12 +1,10 @@
 import React, {useState} from 'react';
-import MixB from '../../assets/pictures/Preview_image_MixB.PNG'
-import portfolio from '../../assets/pictures/portfolio_background_Dutch.png'
 import {ProjectPreviewer} from "../../components/ProjectPreviewer/ProjectPreviewer";
-
+import ProjectsInfo from "../../assets/projectsinfo/ProjectsInfo.json"
 
 export function Projects() {
 
-    const [selectedProject, setSelectedProject] = useState(1);
+    const [selectedProject, setSelectedProject] = useState(0);
 
 
     return (
@@ -15,25 +13,14 @@ export function Projects() {
                 <p className="text-slate-700 max-w-2xl">Welkom op mijn projectpagina! Hier kan je makkelijk de door mijn gemaakte applicaties vinden met een korte beschrijving van de gebruikte technieken. Klik op het plaatje om naar een applicatie te gaan!
                 </p>
             </article>
-
-            {selectedProject <= 1 ? <ProjectPreviewer
+            <ProjectPreviewer
                 selectedProject={selectedProject}
                 setSelectedProject={setSelectedProject}
-                title="MixB!"
-                link="https://deft-narwhal-4151f0.netlify.app"
-                picture={MixB}
-                description="is een cocktail recepten database, ontwikkeld via React, gestyled met gebruik van CSS-modules en navigeerbaar door React-Router-Dom. De recepten zelf worden opgevraagd via GET requests met behulp van Axios naar de CocktailDb API en de inlog- en accountpagina werken dankzij React-Hook-Form."
-            ></ProjectPreviewer> :
-                <ProjectPreviewer
-                 selectedProject={selectedProject}
-                 setSelectedProject={setSelectedProject}
-                 title="Portfolio Bart"
-                 link=""
-                 picture={portfolio}
-                 description="is een portfolio van een jonge enthousiaste Frontend developer, ontwikkeld via React, gestyled met behulp van Tailwind CSS en navigeerbaar via React-Router Dom. Het contactformulier op de Contact pagina werkt via React-Hook-Form en mailt direct naar het mailadres van de developer via mailjs."
-                >
-                </ProjectPreviewer>
-            }
+                title={ProjectsInfo[selectedProject].title}
+                link={ProjectsInfo[selectedProject].link}
+                picture={ProjectsInfo[selectedProject].picture}
+                description={ProjectsInfo[selectedProject].description}
+            ></ProjectPreviewer>
         </section>
     );
 }
