82b5261a8b3b5829013c1feebee67b26f313af1a
BCAvZ <Bartvanzanen@hotmail.com>
2023-10-31 21:33:01 +0100

    Tiny bug fix swapped values on forward and backward buttons around

--- src/components/ProjectPreviewer/ProjectPreviewer.js
+++ src/components/ProjectPreviewer/ProjectPreviewer.js
@@ -25,13 +25,13 @@ export function ProjectPreviewer({selectedProject, setSelectedProject, title, li
             <article className="bg-amber-100 rounded-2xl p-2 grow-0">
                 <h1 className="font-bold flex justify-center">{title}</h1>
                 <div className="flex flex-row justify-evenly items-center">
-                    <button onClick={(e) => addOrReduce(1)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Vorige</button>
+                    <button onClick={(e) => addOrReduce(-1)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Vorige</button>
                     <div className="flex justify-center sm:h-96 sm:w-96 h-2/4 w-2/4 ">
                         <a href={link} className=" ">
                             <img src={imageSelector()} alt="ApplicationPreview" className="w-full h-full object-fill" />
                         </a>
                     </div>
-                    <button onClick={(e) => addOrReduce(-1)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Volgende</button>
+                    <button onClick={(e) => addOrReduce(1)} className="bg-amber-400 w-auto max-h-8 font-bold rounded-3xl p-1 ">Volgende</button>
                 </div>
                 <p className=" max-w-2xl italic">
                     <a href={link} className="underline">{title}</a> {description}
